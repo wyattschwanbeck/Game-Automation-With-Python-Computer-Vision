f9a7bb6fc9dc9cf6e902693f4b50bab595088196
Wyatt Schwanbeck <wyattschwanbeck@gmail.com>
2022-05-10 20:37:46 -0400

    Update main to reflect 10X10 grid and gradient class

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from tkinter import *
-from Collect_Health_Data import *
-inv_dropdowns = [["", "", "", "", "", "", "", "", "", ""],
+#from Collect_Health_Data import *
+dig_dropdowns = [["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
               ["", "", "", "", "", "", "", "", "", ""],
               ["", "", "", "", "", "", "", "", "", ""],
               ["", "", "", "", "", "", "", "", "", ""]]
-listener = Custom_Listener()
+
+#listener = Custom_Listener()
 
 master = Tk()
 mainframe = Frame(master)
@@ -13,18 +20,17 @@ mainframe.columnconfigure(0, weight = 10)
 mainframe.rowconfigure(0, weight = 4)
 #mainframe.pack(pady = 100, padx = 100)
 
-master.title("D2 Inventory Capture for Neural Network Training")
+master.title("mAEGdIG")
 
-choices = [ '', 'misc',
-            'minor health','minor mana',
-            'light health','light mana',
-            'mana', 'health',
-            'greater health','greater mana',
-            'super health','super mana',
-            'rejuv', 'full rejuv'
-            ]
+choices = ["red" ,"organge", "yellow", "green", ""]
 
-inv_labels = [["", "", "", "", "", "", "", "", "", ""],
+dig_labels = [["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
+              ["", "", "", "", "", "", "", "", "", ""],
               ["", "", "", "", "", "", "", "", "", ""],
               ["", "", "", "", "", "", "", "", "", ""],
               ["", "", "", "", "", "", "", "", "", ""]]
@@ -52,21 +58,57 @@ def set_detected_inv():
 #variable = StringVar(master)
 #variable.set("one") # default value
 
-for y in range(0,4):
+for y in range(0,10):
     for x in range(0, 10):
-        inv_labels[y][x] = StringVar(master)
-        inv_dropdowns[y][x] = OptionMenu(master, inv_labels[y][x], *choices)
-        inv_dropdowns[y][x].config(width=6)
-        inv_dropdowns[y][x].grid(row=y, column=x)
-        
-capture_button = Button(master, text = "capture inventory", command=pass_labels)
-capture_button.grid(row=5, column=1)
+        dig_labels[y][x] = StringVar(master)
+        dig_dropdowns[y][x] = OptionMenu(master, dig_labels[y][x], *choices)
+        dig_dropdowns[y][x].config(width=6)
+        dig_dropdowns[y][x].grid(row=y, column=x)
 
-detect_inv = Button(master, text = "detect inventory", command=set_detected_inv)
-detect_inv.grid(row=5, column=4)
 
-reset_button = Button(master, text = "reset inventory", command=reset_labels)
-reset_button.grid(row=5, column=8)
+class Gradient(object):
+    
+    Red = 0
+    Green = 0
+    Blue = 0
+
+    def __init__(self, percentSure):
+        red_min = 0.0 
+        orange_min = .33
+        yellow_min = .66
+        green_min = 1
+        red = [255,0,0]
+        orange = [255, 127, 0]
+        yellow = [255,255,0]
+        green = [0,255,0]
+        if(percentSure<=orange_min):
+            Red = 255
+            Green= _CalculateRGB((orange_min-percentSure)/(orange_min-red_min), red, orange, red_min, orange_min)
+            Blue = 0
+            
+        elif(percentSure<yellow_min):
+            Red=255
+            Green = 255
+            Blue = _CalculateColor((yellow_min-percentSure)/(yellow_min-orange_min),orange, yellow, orange_min, yellow_min)
+        elif(percentSure<green_min):
+            Red = _CalculateColor((green_min-percentSure)/(green_min-orange_min),yellow,green,yellow_min, green_min)
+            Green= 255
+            Blue = 0
+
+    def _CalculateColor(self,percentChange, colorFrom, colorTo, colorFrom_Min, colorTo_Min):
+            
+
+                
+
+
+#capture_button = Button(master, text = "capture inventory", command=pass_labels)
+#capture_button.grid(row=5, column=1)
+
+#detect_inv = Button(master, text = "detect inventory", command=set_detected_inv)
+#detect_inv.grid(row=5, column=4)
+
+#reset_button = Button(master, text = "reset inventory", command=reset_labels)
+#reset_button.grid(row=5, column=8)
 
 
 #
