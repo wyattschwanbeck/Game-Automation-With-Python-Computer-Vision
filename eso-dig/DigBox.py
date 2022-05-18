4df45fa349a075f9d472c9540aea57261ee4ea6e
Maegan Johnson <m.johnson0271@gmail.com>
2022-05-17 20:06:40 -0400

    Isolate DigBox functionality into its file, change all labels to show blank and all color changes to affect background and active background (hover color)

--- eso-dig/DigBox.py
+++ eso-dig/DigBox.py
@@ -1,13 +1,29 @@
 from Gradient import  *
-from tkinter import StringVar
+from tkinter import StringVar, OptionMenu
+from PerspectiveGrid import *
+
+choices = ["red" ,"orange", "yellow", "green", "unset"]
 
 class DigBox (object):
      # String - red, orange, yellow, green, none
 
-    def __init__(self, probability=.5):
+    def __init__(self, master, probability=.5, x=None, y=None):
+
         self.known_color = "unset"
          
-        self.probability = .5
+        self.probability = probability
+
+        label = StringVar(master)
+        label.trace("w", lambda var, index, mode: self._setColorFromLabel())
+        self.tkinterLabel = label
+
+        option = OptionMenu(master, self.tkinterLabel, *choices)
+        option.config(width=3)
+        if (x != None and y != None):
+            option.grid(row=y, column=x)
+        
+        self.tkinterOption = option
+
 
     def GetFrameColor(self):
         return Gradient(0 if self.RuledOut else 1 if self.SureThing else self.probability)
@@ -29,16 +45,24 @@ class DigBox (object):
             return "#%02x%02x%02x" % (255,255,255)
 
     def IsRuledOut(self):
-        if(self.known_color == "unset" or self.known_color == "green"):
-            return False
-        else:
-            return True
+        return not (self.known_color == "unset" or self.known_color == "green")
 
         
     def IsSureThing(self):
-        if(self.known_color == "green"):
-            return True
-        else:
-            return False
+        return self.known_color == "green"
+
+    def setColor(self, color):
+        self.known_color = color
+        self._setButtonColor()
+
+    def _setColorFromLabel(self):
+        self.known_color = self.tkinterLabel.get()
+        self.tkinterLabel.set("")
+        self._setButtonColor()
+
+    def _setButtonColor(self):
+        colorHex = self.GetButtonColor()
+        self.tkinterOption.config(background=colorHex, activebackground=colorHex)
 
 
+   
